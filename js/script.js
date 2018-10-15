0631e0c5a7efb70d047141e618b420d2602f4931
Sophia Barclay <barclay.sophia@gmail.com>
2018-10-15 18:33:30 +0100

    3 ghosts and pacman moving, collision sorted

--- js/script.js
+++ js/script.js
@@ -13,6 +13,11 @@ let pacmanCurrentX = 1;
 let pacmanCurrentY = 1;
 let ghostCurrentX = 9;
 let ghostCurrentY = 9;
+let ghost2CurrentX = 1;
+let ghost2CurrentY = 9;
+let ghost3CurrentX = 9;
+let ghost3CurrentY = 1;
+// let coinCounter = 0;
 
 // Create world
 function createWorld() {
@@ -44,18 +49,36 @@ function createWorld() {
     }
   }
 }
-
 createWorld();
+
+// CHECK WIN
+// function checkSpaceNumber() {
+//     setInterval(() => {
+//
+//     }, 500);
+//   }
+//   console.log();
+
+function showPacman() {
+  const pacmanCurrentPosition = document.querySelector(`div[rowid="${pacmanCurrentY}"][columnid="${pacmanCurrentX}"]`);
+  // console.log(pacmanCurrentPosition);
+  pacmanCurrentPosition.classList.add('pacmanClass');
+}
+showPacman();
 
 // Create Pacman
 function movePacman(keyNumber) {
   switch(keyNumber.keyCode) {
     case 37:
-      console.log('left key');
+      // console.log('left key');
+      console.log(pacmanCurrentX, pacmanCurrentY);
       if (pacmanCurrentX === 1 && pacmanCurrentY === 5) {
         pacmanCurrentX--;
       } else if (pacmanCurrentX === 0 && pacmanCurrentY === 5) {
-        pacmanCurrentX === 10;
+        console.log('WE ARE IN THE UPSIDE DOWN');
+        pacmanCurrentX = 10;
+        pacmanCurrentY = 5;
+        console.log(pacmanCurrentX, pacmanCurrentY);
       } else if (pacmanCurrentX === 1
         || pacmanCurrentX === 6 && pacmanCurrentY ===3
         || pacmanCurrentX === 6 && pacmanCurrentY ===4
@@ -68,7 +91,7 @@ function movePacman(keyNumber) {
       }
       break;
     case 38:
-      console.log('up key');
+      // console.log('up key');
       if (pacmanCurrentY === 1 || pacmanCurrentX === 5 && pacmanCurrentY === 8
       ||  pacmanCurrentX === 0 && pacmanCurrentY === 5 || pacmanCurrentX === 10 && pacmanCurrentY === 5) {
         break;
@@ -77,10 +100,14 @@ function movePacman(keyNumber) {
       }
       break;
     case 39:
-      console.log('right key');
+      // console.log('right key');
       if (pacmanCurrentX === 9 && pacmanCurrentY === 5) {
         pacmanCurrentX++;
       } else if (pacmanCurrentX === 10 && pacmanCurrentY === 5) {
+        console.log('WE ARE IN THE UPSIDE DOWN');
+        pacmanCurrentX = 0;
+        pacmanCurrentY = 5;
+        console.log(pacmanCurrentX, pacmanCurrentY);
         pacmanCurrentX === 0;
       } else if (pacmanCurrentX === 9
         || pacmanCurrentX === 4 && pacmanCurrentY ===3
@@ -94,7 +121,7 @@ function movePacman(keyNumber) {
       }
       break;
     case 40:
-      console.log('down key');
+      // console.log('down key');
       if (pacmanCurrentY === 9 || pacmanCurrentX === 5 && pacmanCurrentY === 2
         ||  pacmanCurrentX === 0 && pacmanCurrentY === 5 || pacmanCurrentX === 10 && pacmanCurrentY === 5) {
         break;
@@ -109,90 +136,224 @@ function movePacman(keyNumber) {
     const coin = document.createElement('div');
     coin.classList.add('coinClass');
     pacmanCurrentDiv.classList.add('coinClass');
+    // coinCounter++;
+    // console.log('Coin counter ------->', coinCounter);
+    // if (coinCounter === 78) {
+    //   window.confirm('WOOOHOOOYou have won!!');
+    // }
   }
-  const pacmanCurrentPosition = document.querySelector(`div[rowid="${pacmanCurrentY}"][columnid="${pacmanCurrentX}"]`);
-  console.log(pacmanCurrentPosition);
-  pacmanCurrentPosition.classList.add('pacmanClass');
+  showPacman();
 }
 
-// Create ghost
+// Create ghosts
+// Ghost 1
 function moveGhost() {
   const x = Math.floor((Math.random() * 4));
   if (x === 0) {
-    console.log('Ghost left');
+    // console.log('Ghost left');
     if (ghostCurrentX === 1
     || ghostCurrentX === 6 && ghostCurrentY === 3
     || ghostCurrentX === 6 && ghostCurrentY === 4
     || ghostCurrentX === 6 && ghostCurrentY === 5
     || ghostCurrentX === 6 && ghostCurrentY === 6
     || ghostCurrentX === 6 && ghostCurrentY === 7) {
-      console.log('BRICK WALL');
+      // console.log('BRICK WALL');
       ghostCurrentX++;
     } else {
       ghostCurrentX--;
     }
   } else if (x === 1) {
     if (ghostCurrentY === 1 || ghostCurrentX === 5 && ghostCurrentY === 8) {
-      console.log('BRICK WALL');
+      // console.log('BRICK WALL');
       ghostCurrentY++;
     } else {
       ghostCurrentY--;
     }
   } else if (x === 2) {
-    console.log('Ghost right');
+    // console.log('Ghost right');
     if (ghostCurrentX === 9
     || ghostCurrentX === 4 && ghostCurrentY ===3
     || ghostCurrentX === 4 && ghostCurrentY ===4
     || ghostCurrentX === 4 && ghostCurrentY ===5
     || ghostCurrentX === 4 && ghostCurrentY ===6
     || ghostCurrentX === 4 && ghostCurrentY ===7) {
-      console.log('BRICK WALL');
+      // console.log('BRICK WALL');
       ghostCurrentX--;
     } else {
       ghostCurrentX++;
     }
   } else if (x === 3) {
-    console.log('Ghost down');
+    // console.log('Ghost down');
     if (ghostCurrentY === 9 || ghostCurrentX === 5 && ghostCurrentY === 2) {
-      console.log('BRICK WALL');
+      // console.log('BRICK WALL');
       ghostCurrentY--;
     } else {
       ghostCurrentY++;
     }
   }
-  if (ghostCurrentX === pacmanCurrentX && ghostCurrentY === pacmanCurrentY){
-    console.log('Collision!!!!!!!!!!!!!');
-    alert('You have lost');
-  }
+
   const ghostCurrentDiv = document.querySelector('.ghostClass');
   if (ghostCurrentDiv) {
     ghostCurrentDiv.classList.remove('ghostClass');
   }
   const ghostCurrentPosition = document.querySelector(`div[rowid="${ghostCurrentY}"][columnid="${ghostCurrentX}"]`);
-  console.log(ghostCurrentPosition);
+  // console.log(ghostCurrentPosition);
   ghostCurrentPosition.classList.add('ghostClass');
 }
 
 function moveGhostInterval() {
-  setInterval(moveGhost, 1000);
+  setInterval(moveGhost, 500);
 }
 moveGhostInterval();
 
-// Detect collision
-// function detectCollision() {
-//   if (ghostCurrentX === pacmanCurrentX && ghostCurrentY === pacmanCurrentY){
-//     console.log('Collision!!!!!!!!!!!!!');
-//   }
-// }
+// Ghost 2
+function moveGhost2() {
+  const x = Math.floor((Math.random() * 4));
+  if (x === 0) {
+    // console.log('Ghost2 left');
+    if (ghost2CurrentX === 1
+    || ghost2CurrentX === 6 && ghost2CurrentY === 3
+    || ghost2CurrentX === 6 && ghost2CurrentY === 4
+    || ghost2CurrentX === 6 && ghost2CurrentY === 5
+    || ghost2CurrentX === 6 && ghost2CurrentY === 6
+    || ghost2CurrentX === 6 && ghost2CurrentY === 7) {
+      // console.log('BRICK WALL');
+      ghost2CurrentX++;
+    } else {
+      ghost2CurrentX--;
+    }
+  } else if (x === 1) {
+    if (ghost2CurrentY === 1 || ghost2CurrentX === 5 && ghost2CurrentY === 8) {
+      // console.log('BRICK WALL');
+      ghost2CurrentY++;
+    } else {
+      ghost2CurrentY--;
+    }
+  } else if (x === 2) {
+    // console.log('Ghost2 right');
+    if (ghost2CurrentX === 9
+    || ghost2CurrentX === 4 && ghost2CurrentY ===3
+    || ghost2CurrentX === 4 && ghost2CurrentY ===4
+    || ghost2CurrentX === 4 && ghost2CurrentY ===5
+    || ghost2CurrentX === 4 && ghost2CurrentY ===6
+    || ghost2CurrentX === 4 && ghost2CurrentY ===7) {
+      // console.log('BRICK WALL');
+      ghost2CurrentX--;
+    } else {
+      ghost2CurrentX++;
+    }
+  } else if (x === 3) {
+    // console.log('Ghost2 down');
+    if (ghost2CurrentY === 9 || ghost2CurrentX === 5 && ghost2CurrentY === 2) {
+      // console.log('BRICK WALL');
+      ghost2CurrentY--;
+    } else {
+      ghost2CurrentY++;
+    }
+  }
+
+  const ghost2CurrentDiv = document.querySelector('.ghost2Class');
+  if (ghost2CurrentDiv) {
+    ghost2CurrentDiv.classList.remove('ghost2Class');
+  }
+  const ghost2CurrentPosition = document.querySelector(`div[rowid="${ghost2CurrentY}"][columnid="${ghost2CurrentX}"]`);
+  // console.log(ghost2CurrentPosition);
+  ghost2CurrentPosition.classList.add('ghost2Class');
+}
+
+function moveGhost2Interval() {
+  setInterval(moveGhost2, 500);
+}
+moveGhost2Interval();
+
+// Ghost 3
+function moveGhost3() {
+  const x = Math.floor((Math.random() * 4));
+  if (x === 0) {
+    // console.log('Ghost3 left');
+    if (ghost3CurrentX === 1
+    || ghost3CurrentX === 6 && ghost3CurrentY === 3
+    || ghost3CurrentX === 6 && ghost3CurrentY === 4
+    || ghost3CurrentX === 6 && ghost3CurrentY === 5
+    || ghost3CurrentX === 6 && ghost3CurrentY === 6
+    || ghost3CurrentX === 6 && ghost3CurrentY === 7) {
+      // console.log('BRICK WALL');
+      ghost3CurrentX++;
+    } else {
+      ghost3CurrentX--;
+    }
+  } else if (x === 1) {
+    if (ghost3CurrentY === 1 || ghost3CurrentX === 5 && ghost3CurrentY === 8) {
+      // console.log('BRICK WALL');
+      ghost3CurrentY++;
+    } else {
+      ghost3CurrentY--;
+    }
+  } else if (x === 2) {
+    // console.log('Ghost3 right');
+    if (ghost3CurrentX === 9
+    || ghost3CurrentX === 4 && ghost3CurrentY ===3
+    || ghost3CurrentX === 4 && ghost3CurrentY ===4
+    || ghost3CurrentX === 4 && ghost3CurrentY ===5
+    || ghost3CurrentX === 4 && ghost3CurrentY ===6
+    || ghost3CurrentX === 4 && ghost3CurrentY ===7) {
+      // console.log('BRICK WALL');
+      ghost3CurrentX--;
+    } else {
+      ghost3CurrentX++;
+    }
+  } else if (x === 3) {
+    // console.log('Ghost3 down');
+    if (ghost3CurrentY === 9 || ghost3CurrentX === 5 && ghost3CurrentY === 2) {
+      // console.log('BRICK WALL');
+      ghost3CurrentY--;
+    } else {
+      ghost3CurrentY++;
+    }
+  }
+
+  const ghost3CurrentDiv = document.querySelector('.ghost3Class');
+  if (ghost3CurrentDiv) {
+    ghost3CurrentDiv.classList.remove('ghost3Class');
+  }
+  const ghost3CurrentPosition = document.querySelector(`div[rowid="${ghost3CurrentY}"][columnid="${ghost3CurrentX}"]`);
+  // console.log(ghost3CurrentPosition);
+  ghost3CurrentPosition.classList.add('ghost3Class');
+}
+
+function moveGhost3Interval() {
+  setInterval(moveGhost3, 500);
+}
+moveGhost3Interval();
+
+// Check Collision
+function checkCollision() {
+  setInterval(() => {
+    if (ghostCurrentX === pacmanCurrentX && ghostCurrentY === pacmanCurrentY
+    || ghost2CurrentX === pacmanCurrentX && ghost2CurrentY === pacmanCurrentY
+    || ghost3CurrentX === pacmanCurrentX && ghost3CurrentY === pacmanCurrentY){
+      // console.log('Collision!!!!!!!!!!!!!');
+      window.confirm('You have lost. Would you like to play again?');
+      if (confirm('You have lost. Would you like to play again?')) {
+        // console.log('NEW GAME');
+        location.reload();
+      }
+    }
+  }, 100);
+}
+checkCollision();
+
+// function checkIfPacmanWins() {
 //
-// detectCollision();
+// }
+// checkIfPacmanWins();
 
 // NOTE Things to do:
-// 1. When all divs are coin pacman wins
-// 2. Pacman and ghost colide
-// 3. Make pacman and ghost appear from start
-// 4. Offstage / onstage
-// 5.
+// 1. When all divs are coin pacman wins - ! 78 Counter
+// 3. Make pacman object with two properties x and y
+// 4. Randomly generate grid
+// 5. level 1 speed , leves walls [{7,8}, ]
+// 6. Stop ghosts colliding
 
 // NOTE Problems
 // 1. SASS
